103901b359534ff5c2bd9272eb0d07b00a2eb3ae
TheFoxyManuel <TheFoxyManuel@proton.me>
2022-10-29 00:12:48 +0200

    Projekt

---
+++ fox.js
@@ -0,0 +1,7 @@
+function openGithub() {
+    window.open('https://github.com/thefoxymanuel', '_blank').focus();
+}
+
+function showContact() {
+    window.alert("Du kannst mich bei Github kontaktieren :3 Oder bei Twitter ")
+}
